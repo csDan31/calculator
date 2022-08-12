6d77a7d40a25945442da9ae848df005dd61c9325
csDan31 <csDan31@gmail.com>
2022-08-11 23:44:12 -0400

    added button even for numbers and clear button functionality

--- scripts.js
+++ scripts.js
@@ -11,3 +11,42 @@ const operate = function(operator,num1,num2){
         return operatorObj[operator](num1,num2);
     }
 }
+
+let displayVar = 0;
+
+// button event handler //
+
+
+// clear and delete buttons //
+let clearBtn = document.getElementById('clear');
+clearBtn.addEventListener('click', ()=> {
+    displayVar = 0;
+    currentDisplay.textContent = displayVar;
+})
+
+let deleteBtn = document.getElementById('delete');
+
+// number buttons //
+
+let numberBtns = document.getElementById('btns');
+numberBtns.addEventListener('click', (event) => {
+    const isButton = event.target.nodeName === 'BUTTON';
+
+    if(!isButton) {
+        return;
+    }
+    console.log(event.target.textContent);
+
+    displayVar += event.target.textContent;
+    currentDisplay.textContent = Number(displayVar);
+    return displayVar;
+})
+
+// operator buttons //
+let operatorBtn = document.getElementById('operator');
+
+// display handler //
+let currentDisplay = document.querySelector('.display-current');
+currentDisplay.textContent = displayVar;
+
+let prevDisplay = document.querySelector('.display-history');
